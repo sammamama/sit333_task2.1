ff9fcedb912e776562797ae94bd937f4e890429f
sammamama <167903808+sammamama@users.noreply.github.com>
2025-03-24 20:55:06 +1100

    Add files via upload

--- src/main/java/sit707_week2/Main.java
+++ src/main/java/sit707_week2/Main.java
@@ -9,5 +9,6 @@ public class Main
     public static void main( String[] args )
     {
         SeleniumOperations.officeworks_registration_page("https://www.officeworks.com.au/app/identity/create-account");
+        SeleniumOperations.github_registration_page("https://github.com/login");
     }
 }
--- src/main/java/sit707_week2/SeleniumOperations.java
+++ src/main/java/sit707_week2/SeleniumOperations.java
@@ -103,5 +103,46 @@ public class SeleniumOperations {
 		driver.close();	
 	}
 	
+	public static void github_registration_page(String url) {
+		// Step 1: Locate chrome driver folder in the local drive.
+		System.setProperty("webdriver.chrome.driver", "D:/Coding/College/SIT333/Task1.1/driverWorking/chromedriver-win64/chromedriver.exe");
+		
+		// Step 2: Use above chrome driver to open up a chromium browser.
+		System.out.println("Fire up chrome browser.");
+		WebDriver driver = new ChromeDriver();
+		
+		System.out.println("Driver info: " + driver);
+		
+		sleep(2);
+	
+		// Load a webpage in chromium browser.
+		driver.get(url);
+		
+		/*
+		 * How to identify a HTML input field -
+		 * Step 1: Inspect the webpage, 
+		 * Step 2: locate the input field, 
+		 * Step 3: Find out how to identify it, by id/name/...
+		 */
+		
+		// Find first input field which is username
+		WebElement username = driver.findElement(By.id("login_field"));
+		System.out.println("Found element: " + username);
+		// Send first name
+		username.sendKeys("Sammie");
+		
+		
+		WebElement button = driver.findElement(By.className("js-sign-in-button"));
+		System.out.println("Found element: " + button);
+		// Click the button
+		button.click();
+		
+		// Sleep a while
+		sleep(5);
+		
+		// close chrome driver
+		driver.close();	
+	}
+	
 	
 }
